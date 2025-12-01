221df48e7af0b2904d11eaeb37af9bf0ef1d4270
Marius Lindvall <marius@varden.info>
2025-12-01 21:42:08 +0100

    Move supporting info below title and description

--- src/scripts/widescreen-review.ts
+++ src/scripts/widescreen-review.ts
@@ -17,7 +17,7 @@
 // If not, see <https://www.gnu.org/licenses/>.
 
 import { register } from "src/core";
-import { makeChildNode, untilTruthy } from "src/utils";
+import { makeChildNode } from "src/utils";
 
 import "./widescreen-review.css";
 
@@ -29,26 +29,10 @@ export default () => {
     description: "Improves the review interface to be more comfortable on desktop by switching to a three-column layout",
     defaultConfig: {},
     sessionData: {},
-    initialize: (toolbox, logger, config) => {
-      const setupObserver = async () => {
-        const ref = await untilTruthy(() => document.querySelector("body"));
-        const observer = new MutationObserver((mutations) => {
-          for (const mutation of mutations) {
-            for (const node of mutation.addedNodes) {
-              if (node.nodeName === "APP-REVIEW-NEW-B") {
-                updateView((node as HTMLElement).children[0]);
-              }
-            }
-          }
-        });
-        observer.observe(ref, {
-          childList: true,
-          subtree: true,
-        });
-      };
-
+    initialize: (toolbox, logger, _config) => {
       const updateView = (ref: Element) => {
         ref.closest("mat-sidenav-content > .max-w-7xl")?.classList.remove("max-w-7xl");
+        ref.classList.add("oprwsr-root-new");
         logger.info("ref", ref);
         console.log(ref.querySelectorAll("div"));
         const columns = [...ref.children].filter(node => node.tagName === "DIV").map(node => node);
@@ -68,14 +52,24 @@ export default () => {
         c2.classList.remove("review-questions");
         c3.classList.add("flex", "flex-col", "gap-3", "review-questions");
 
+        const supportingBox = document.createElement("div");
+        c2.insertAdjacentElement("afterbegin", supportingBox);
         c2.insertAdjacentElement("afterbegin", c1.querySelector("app-title-and-description-b")!);
+
+        const supportingCard = makeChildNode(supportingBox, "wf-review-card");
+        supportingCard.classList.add("wf-review-card", "card");
+        supportingCard.appendChild(c1.querySelector("app-supporting-info-b .wf-review-card__header")!.cloneNode(true));
+        const cardBody = makeChildNode(supportingCard, "div");
+        cardBody.classList.add("wf-review-card__body");
+        cardBody.appendChild(c1.querySelector("app-supporting-info-b .wf-review-card__body .wf-image-modal + div")!);
+
         for (const question of c2.querySelectorAll("app-question-card")) {
           c3.appendChild(question);
         }
         c3.appendChild(c2.querySelector("app-review-categorization-b")!);
       };
 
-      void setupObserver();
+      toolbox.observeAddedNodes<Element>("APP-REVIEW-NEW-B", (node) => updateView(node.children[0]));
     },
   });
 };
